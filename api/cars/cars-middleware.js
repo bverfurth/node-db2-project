6215823ecd78439b41177c695b2a1bef7e8c931e
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-07 17:17:25 -0400

    wip for middleware checkCarId

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,39 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const { getById, getAll } = require("./cars-model");
+
+const vinValidator = require("vin-validator");
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await getById(req.params.id);
+    if (car) {
+      req.car = car;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: `car with id ${req.params.id} is not found`,
+      });
+    }
+  } catch (err) {
+    next();
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
