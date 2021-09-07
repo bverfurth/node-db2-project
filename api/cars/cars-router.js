8902525ba7576634b3ef2ff721aac4539a8ee889
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-07 18:02:36 -0400

    finished cars-router.js

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,49 @@
-// DO YOUR MAGIC
+const express = require("express");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
+const Cars = require("./cars-model");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.status(200).json(cars);
+  } catch (error) {
+    next();
+  }
+});
+
+router.get("/:id", checkCarId, (req, res) => {
+  res.status(200).json(req.car);
+});
+
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+  async (req, res, next) => {
+    try {
+      const newCar = await Cars.create(req.body);
+      res.status(200).json(newCar);
+    } catch (error) {
+      next();
+    }
+  }
+);
+
+// ESLint-Disable
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    custom: "Uh oh, Something no bueno happened",
+    message: error.message,
+    stack: error.stack,
+  });
+});
+
+module.exports = router;
